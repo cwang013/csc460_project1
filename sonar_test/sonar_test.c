4d71aba63ec9bbae91c7e8d6d8142823b7512c13
Andrew Garber <andrew@andrewgarber.com>
2010-05-15 01:00:57 -0700

    Added sonar_test project.
    This was first generated with the create-project script, then
    sonar_test.c was replaced with the copy provided in the conneX
    resources. The only edit I made to sonar_test.c is that the include
    directives for sonar.h and uart.h assumed these files were located under
    sonar/ and uart/, respectively (i.e. I changed "sonar/sonar.h" to
    "sonar.h").
    
    Note that the makefile sets the COMMON_MODULES variable.
    
    The code currently does not compile.

---
+++ sonar_test/sonar_test.c
@@ -0,0 +1,63 @@
+/*
+ * main.c
+ *
+ *  Created on: 2010-05-12
+ *      Author: lienh
+ */
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include "uart.h"
+#include "sonar.h"
+
+#define     clock8MHz()    CLKPR = _BV(CLKPCE); CLKPR = 0x00;
+
+volatile uint8_t rxflag = 0;
+volatile float distance = 0;
+
+radiopacket_t packet;
+
+void print(uint8_t* output)
+{
+	uint8_t i;
+	for (i = 0; i < 64 && output[i] != 0; i++)
+	{
+		uart_putchar(output[i]);
+	}
+}
+
+int main()
+{
+	// string buffer for printing to UART
+	uint8_t output[128];
+
+	// disable the clock prescaler
+	clock8MHz();
+
+	// disable interrupts during setup
+	cli();
+
+	uart_init(UART_38400);
+	sonar_init();
+
+	// enable interrupts
+	sei();
+
+
+	for (;;)
+	{
+		sonar_trigger(SONAR1);
+		_delay_ms(36);
+		if (sonar_echo_received() == 1) {
+			distance = 0.0171 * sonar_get_distance() -  0.8192;
+			snprintf((char*)output, 128, "distance %d\n\r", (int)distance);
+			print(output);
+		}else {
+			snprintf((char*)output, 128, "sonar failed\n\r");
+			print(output);
+		}
+
+		_delay_ms(1000);
+	}
+
+	return 0;
+}
