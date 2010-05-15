66258d20a31dcfe3b955e3af96e6416f865d0f53
Andrew Garber <andrew@andrewgarber.com>
2010-05-15 16:23:28 -0700

    cleaned up common.mk
    - deleted rules that we don't need
    - added comments
    - deleted useless comments
    - added .PHONY where appropriate
    - renamed "flash" target to "install"

--- common/common.mk
+++ common/common.mk
@@ -1,58 +1,44 @@
 # The make manual: http://www.gnu.org/software/make/manual/make.html
 
-# DEVICE ....... The AVR device you compile for
-# CLOCK ........ Target AVR clock rate in Hertz
-# OBJECTS ...... The object files created from your source files. This list is
-#                usually the same as the list of source files with suffix ".o".
-# PROGRAMMER ... Options to avrdude which define the hardware you use for
-#                uploading to the AVR and the interface where this hardware
-#                is connected.
-
+# PROJECT_NAME is the name of the current directory.
 PROJECT_NAME = $(notdir $(abspath .))
 
 DEVICE     = atmega2560
 CLOCK      = 8000000
 PROGRAMMER = -c jtagmkII -P usb
 
-OBJECTS += $(PROJECT_NAME).o
+OBJECTS = $(PROJECT_NAME).o
 
 vpath %.c ../common
 vpath %.h ../common
 
-# Tune the lines below only if you know what you are doing:
-
 AVRDUDE = avrdude $(PROGRAMMER) -p $(DEVICE)
 COMPILE = avr-gcc -Wall -I../common -Os -DF_CPU=$(CLOCK) -mmcu=$(DEVICE)
 
-# symbolic targets:
+.PHONY: all
 all: $(PROJECT_NAME).hex
 
+# For each FOO in COMMON_MODULES, include ../common/FOO.mk
 ifdef COMMON_MODULES
 include $(addprefix ../common/,$(COMMON_MODULES:=.mk))
 endif
 
+# Note that this implicit rule must come AFTER explicit rules
+# like those defined by the common modules.
 .c.o:
 	$(COMPILE) -c $< -o $@
 
-.c.s:
-	$(COMPILE) -S $< -o $@
-
-flash: all
+.PHONY: install
+install: all
 	$(AVRDUDE) -U flash:w:$(PROJECT_NAME).hex:i
 
+.PHONY: clean
 clean:
 	rm -f *.hex *.elf *.o
 
-# file targets:
 $(PROJECT_NAME).elf: $(OBJECTS)
 	$(COMPILE) -o $(PROJECT_NAME).elf $(OBJECTS)
 
 $(PROJECT_NAME).hex: $(PROJECT_NAME).elf
 	rm -f $(PROJECT_NAME).hex
 	avr-objcopy -j .text -j .data -O ihex $(PROJECT_NAME).elf $(PROJECT_NAME).hex
-# If you have an EEPROM section, you must also create a hex file for the
-# EEPROM and add it to the "flash" target.
-
-# Targets for code debugging and analysis:
-disasm: $(PROJECT_NAME).elf
-	avr-objdump -d $(PROJECT_NAME).elf
