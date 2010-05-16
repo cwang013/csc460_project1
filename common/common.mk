cdb356acbbc878af067c466a154f3e3ed1ec1744
Andrew Garber <andrew@andrewgarber.com>
2010-05-15 18:29:34 -0700

    common.mk now makes us of built-in make rules
    TIP: run make with '-p' to see the database of rules and variables

--- common/common.mk
+++ common/common.mk
@@ -1,30 +1,34 @@
 # The make manual: http://www.gnu.org/software/make/manual/make.html
-
-# PROJECT_NAME is the name of the current directory.
-PROJECT_NAME = $(notdir $(abspath .))
+# A free book on make: http://oreilly.com/catalog/make3/book/
 
 DEVICE     = atmega2560
-CLOCK      = 8000000
+CLOCK      = 8000000 # 8 MHz
 PROGRAMMER = -c jtagmkII -P usb
+
+# PROJECT_NAME is the name of the current directory.
+PROJECT_NAME = $(notdir $(CURDIR))
 
 OBJECTS = $(PROJECT_NAME).o
 
+CC = avr-gcc
+CFLAGS = -Wall -gdwarf-2 -std=gnu99 -Os -funsigned-char -funsigned-bitfields \
+		 -fpack-struct -fshort-enums -DF_CPU=$(CLOCK)
+TARGET_ARCH = -mmcu=$(DEVICE)
+LDFLAGS = -Wall
+
 AVRDUDE = avrdude $(PROGRAMMER) -p $(DEVICE)
-COMPILE = avr-gcc -Wall -I../common -Os -DF_CPU=$(CLOCK) -mmcu=$(DEVICE)
 
 .PHONY: all
 all: $(PROJECT_NAME).hex
 
 ifdef COMMON_MODULES
-# Add common directory to search path for *.[ch] files.
+# Add the common directory to various search paths.
 vpath %.c ../common
 vpath %.h ../common
+CFLAGS += -I../common
 # Include ../common/FOO.mk for each FOO in COMMON_MODULES.
 include $(addprefix ../common/,$(COMMON_MODULES:=.mk))
 endif
-
-%.o: %.c
-	$(COMPILE) -c $< -o $@
 
 .PHONY: install
 install: all
@@ -32,11 +36,12 @@ install: all
 
 .PHONY: clean
 clean:
-	rm -f *.hex *.elf *.o
+	$(RM) *.hex *.elf *.o
 
 $(PROJECT_NAME).elf: $(OBJECTS)
-	$(COMPILE) -o $(PROJECT_NAME).elf $(OBJECTS)
+	$(LINK.o) $^ -o $@
 
 $(PROJECT_NAME).hex: $(PROJECT_NAME).elf
-	rm -f $(PROJECT_NAME).hex
-	avr-objcopy -j .text -j .data -O ihex $(PROJECT_NAME).elf $(PROJECT_NAME).hex
+	$(RM) $@
+	avr-objcopy -j .text -j .data -O ihex $< $@
+
