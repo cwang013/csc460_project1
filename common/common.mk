87e147b5cdf50125ece5bbff96441dac13979d49
Andrew Garber <andrew@andrewgarber.com>
2010-05-15 19:28:08 -0700

    re-arranged variable declarations in common.mk
    Also set SHELL to /bin/bash (the default is /bin/sh). This means that
    commands inside a make rule will now be executed using bash.

--- common/common.mk
+++ common/common.mk
@@ -1,22 +1,24 @@
 # The make manual: http://www.gnu.org/software/make/manual/make.html
 # A free book on make: http://oreilly.com/catalog/make3/book/
 
-DEVICE     = atmega2560
-CLOCK      = 8000000 # 8 MHz
-PROGRAMMER = -c jtagmkII -P usb
-
 # PROJECT_NAME is the name of the current directory.
 PROJECT_NAME = $(notdir $(CURDIR))
 
 OBJECTS = $(PROJECT_NAME).o
 
+DEVICE     = atmega2560
+CLOCK      = 8000000 # 8 MHz
+PROGRAMMER = -c jtagmkII -P usb
+
+AVRDUDE = avrdude $(PROGRAMMER) -p $(DEVICE)
+
+# These variables are used by make's built-in rules.
 CC = avr-gcc
 CFLAGS = -Wall -gdwarf-2 -std=gnu99 -Os -funsigned-char -funsigned-bitfields \
 		 -fpack-struct -fshort-enums -DF_CPU=$(CLOCK)
 TARGET_ARCH = -mmcu=$(DEVICE)
 LDFLAGS = -Wall
-
-AVRDUDE = avrdude $(PROGRAMMER) -p $(DEVICE)
+SHELL = /bin/bash
 
 .PHONY: all
 all: $(PROJECT_NAME).hex
